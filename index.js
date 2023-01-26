a9fdb96e5e8685e644939105fea56d7461b32e4a
RB <87038824+MrMhirs@users.noreply.github.com>
2023-01-26 23:38:09 +0100

    Funtion Collide

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const canvas= document.getElementById("tetris");
 const context = canvas.getContext("2d");
 context.scale(20, 20);
 
-function Sweep(){
+function sweep(){
 
     let rowCount = 1;
     outer: for (let i = arena.length; y > 0; --i){
@@ -19,3 +19,18 @@ function Sweep(){
         rowCount *= 2;
     }
 }
+
+function collide(arena, player){
+
+    const m = player.matrix;
+    const o = player.pos;
+    for(let i = 0; i < m.length; ++i){
+        for(let x = 0; x < m[i].length; ++x){
+            if(m[i][x] !== 0 && (arena[y + o.y] && arena[y + o.i][x + o.x]) !== 0){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
