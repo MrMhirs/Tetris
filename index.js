b0d2fb3ae91ef9c4fb4db4be300ca7f2a96ddbbc
RB <87038824+MrMhirs@users.noreply.github.com>
2023-01-27 12:49:39 +0100

    Colors

--- index.js
+++ index.js
@@ -201,4 +201,17 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
+const colors = [
+    null,
+    "#ff0d72",
+    "#0dc2ff",
+    "#0dff72",
+    "#f538ff",
+    "#ff8e0d",
+    "#ffe138",
+    "#3877ff",
+];
+
+
+
 
